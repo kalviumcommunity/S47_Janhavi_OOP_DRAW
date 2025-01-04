98c7e1f30d113ef9599e5a2f7e8ed91459daa58a
janhavichauhan <chauhanjanhavi45@gmail.com>
2025-01-04 18:23:17 +0530

    DIP implemented

--- ArtExhibition.java
+++ ArtExhibition.java
@@ -10,6 +10,7 @@ public class ArtExhibition {
     }
 
     static abstract class Artwork {
+        
         private String title;
         private String artist;
         private int year;
@@ -19,6 +20,8 @@ public class ArtExhibition {
             this.artist = artist;
             this.year = year;
         }
+
+
 
         public String getTitle() {
             return title;
